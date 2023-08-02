1f149cc54aeeacf355f87f540141d3f30e1d66e5
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-08-02 20:55:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+// const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
