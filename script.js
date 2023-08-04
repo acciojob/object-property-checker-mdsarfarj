799909a6ac455b6736ef66ae26f60a210f3c7280
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-08-04 23:11:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-// const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
-function hasKey(key) {
-  //   write your code here
-	const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
-function hasKey(key) {
-  return sampleObject.hasOwnProperty(key);
+function hasKey(key) { 
+  return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
-}
-
+ 
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
